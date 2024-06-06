12cefbe97ab6c3f37f78a1a40b3ebd1d1fad72f1
piacobelli14 <dinolabsauthentication@outlook.com>
2024-06-06 13:47:18 -0500

    update

---
+++ NightingaleBookkeeper/NightingaleBookkeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightingaleBookkeeperApp.swift
+//  NightingaleBookkeeper
+//
+//  Created by Peter Iacobelli on 6/6/24.
+//
+
+import SwiftUI
+
+@main
+struct NightingaleBookkeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
