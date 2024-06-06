0e5cb3ecb7a5ad9d13406dad45ccc233427da855
piacobelli14 <dinolabsauthentication@outlook.com>
2024-06-06 17:02:01 -0500

    update

--- NightingaleBookkeeper/Views/BookManage/DeviceManage.swift
+++ NightingaleBookkeeper/Views/BookManage/DeviceManage.swift
@@ -33,7 +33,8 @@ struct DeviceManage: View {
     @State private var errorMessage: String? = nil
     
     @State private var deviceInfo: [WatchData] = []
-    var columns: [GridItem] = Array(repeating: .init(.flexible()), count: 2)
+    @State private var columns: [GridItem] = Array(repeating: .init(.flexible()), count: 2)
+    @State private var selectedDeviceID: String = ""
     
     var body: some View {
         GeometryReader { geometry in
@@ -184,86 +185,90 @@ struct DeviceManage: View {
         }
     }
     private func deviceCell(for device: WatchData, geometry: GeometryProxy) -> some View {
-        VStack {
-            HStack {
-                VStack(alignment: .trailing) {
-                    Text("Device Type: ")
-                        .font(.system(size: geometry.size.height * 0.012, weight: .heavy))
-                        .foregroundColor(Color.black)
-                    
-                    Text("Device ID: ")
-                        .font(.system(size: geometry.size.height * 0.012, weight: .heavy))
-                        .foregroundColor(Color.black)
-                        .padding(.top, geometry.size.height * 0.002)
-                        
-                }
-                VStack(alignment: .leading) {
-                    Text(device.devType)
-                        .font(.system(size: geometry.size.height * 0.012, weight: .semibold))
-                        .foregroundColor(Color.black)
-                        .italic()
-                    
-                    Text(device.devID)
-                        .font(.system(size: geometry.size.height * 0.012, weight: .semibold))
-                        .foregroundColor(Color.black)
-                        .italic()
-                        .padding(.top, geometry.size.height * 0.002)
-                }
-            }
-            
-            WatchView(devType: "AppleWatch")
-
-            
+        Button(action: {
+            selectedDeviceID = device.devID
+        }) {
             VStack {
                 HStack {
-                    Spacer()
-                    Text(device.assignedTo != "None" ? "In Use: " : "Available")
-                        .font(.system(size: geometry.size.height * 0.01, weight: .heavy))
-                    Text(device.assignedTo != "None" ? device.assignedTo : "")
-                        .font(.system(size: geometry.size.height * 0.01, weight: .regular))
-                    Spacer()
+                    VStack(alignment: .trailing) {
+                        Text("Device Type: ")
+                            .font(.system(size: geometry.size.height * 0.012, weight: .heavy))
+                            .foregroundColor(Color.black)
+                        
+                        Text("Device ID: ")
+                            .font(.system(size: geometry.size.height * 0.012, weight: .heavy))
+                            .foregroundColor(Color.black)
+                            .padding(.top, geometry.size.height * 0.002)
+                        
+                    }
+                    VStack(alignment: .leading) {
+                        Text(device.devType)
+                            .font(.system(size: geometry.size.height * 0.012, weight: .semibold))
+                            .foregroundColor(Color.black)
+                            .italic()
+                        
+                        Text(device.devID)
+                            .font(.system(size: geometry.size.height * 0.012, weight: .semibold))
+                            .foregroundColor(Color.black)
+                            .italic()
+                            .padding(.top, geometry.size.height * 0.002)
+                    }
                 }
-                .frame(width: geometry.size.width * 0.3)
-                .padding(.vertical, geometry.size.height * 0.01)
-                .background(device.assignedTo != "None" ? Color(hex: 0x5BBA6F).opacity(0.1) : Color(hex: 0xBD9B19).opacity(0.1))
-                .foregroundColor(device.assignedTo != "None" ? Color(hex: 0x6BC17D).opacity(1) : Color(hex: 0xBD9B19))
-                .overlay(
-                    RoundedRectangle(cornerRadius: geometry.size.width * 0.01)
-                        .stroke(device.assignedTo != "None" ? Color(hex: 0x6BC17D).opacity(1) : Color(hex: 0xBD9B19), lineWidth: geometry.size.width * 0.004)
-                )
                 
-                let batteryStatusText = Int(device.devBattery) ?? 0 < 20 ? "Not Charged: " : "Charged: "
-                let batteryStatusPrimaryColor = Int(device.devBattery) ?? 0 < 20 ? Color(hex: 0xE54B4B).opacity(1) : Color(hex: 0x6BC17D).opacity(1)
-                let batteryStatusSecondaryColor = Int(device.devBattery) ?? 0 < 20 ? Color(hex: 0xE54B4B).opacity(0.1) : Color(hex: 0x5BBA6F).opacity(0.1)
-                HStack {
-                    Spacer()
-                    Text(batteryStatusText)
-                        .font(.system(size: geometry.size.height * 0.01, weight: .heavy))
-                    Text("\(device.devBattery)%")
-                        .font(.system(size: geometry.size.height * 0.01, weight: .regular))
-                    Spacer()
+                WatchView(devType: "AppleWatch")
+                
+                
+                VStack {
+                    HStack {
+                        Spacer()
+                        Text(device.assignedTo != "None" ? "In Use: " : "Available")
+                            .font(.system(size: geometry.size.height * 0.01, weight: .heavy))
+                        Text(device.assignedTo != "None" ? device.assignedTo : "")
+                            .font(.system(size: geometry.size.height * 0.01, weight: .regular))
+                        Spacer()
+                    }
+                    .frame(width: geometry.size.width * 0.3)
+                    .padding(.vertical, geometry.size.height * 0.01)
+                    .background(device.assignedTo != "None" ? Color(hex: 0x5BBA6F).opacity(0.1) : Color(hex: 0xBD9B19).opacity(0.1))
+                    .foregroundColor(device.assignedTo != "None" ? Color(hex: 0x6BC17D).opacity(1) : Color(hex: 0xBD9B19))
+                    .overlay(
+                        RoundedRectangle(cornerRadius: geometry.size.width * 0.01)
+                            .stroke(device.assignedTo != "None" ? Color(hex: 0x6BC17D).opacity(1) : Color(hex: 0xBD9B19), lineWidth: geometry.size.width * 0.004)
+                    )
+                    
+                    let batteryStatusText = Int(device.devBattery) ?? 0 < 20 ? "Not Charged: " : "Charged: "
+                    let batteryStatusPrimaryColor = Int(device.devBattery) ?? 0 < 20 ? Color(hex: 0xE54B4B).opacity(1) : Color(hex: 0x6BC17D).opacity(1)
+                    let batteryStatusSecondaryColor = Int(device.devBattery) ?? 0 < 20 ? Color(hex: 0xE54B4B).opacity(0.1) : Color(hex: 0x5BBA6F).opacity(0.1)
+                    HStack {
+                        Spacer()
+                        Text(batteryStatusText)
+                            .font(.system(size: geometry.size.height * 0.01, weight: .heavy))
+                        Text("\(device.devBattery)%")
+                            .font(.system(size: geometry.size.height * 0.01, weight: .regular))
+                        Spacer()
+                    }
+                    .frame(width: geometry.size.width * 0.3)
+                    .padding(.vertical, geometry.size.height * 0.01)
+                    .background(batteryStatusSecondaryColor)
+                    .foregroundColor(batteryStatusPrimaryColor)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: geometry.size.width * 0.01)
+                            .stroke(batteryStatusPrimaryColor, lineWidth: geometry.size.width * 0.004)
+                    )
                 }
-                .frame(width: geometry.size.width * 0.3)
-                .padding(.vertical, geometry.size.height * 0.01)
-                .background(batteryStatusSecondaryColor)
-                .foregroundColor(batteryStatusPrimaryColor)
-                .overlay(
-                    RoundedRectangle(cornerRadius: geometry.size.width * 0.01)
-                        .stroke(batteryStatusPrimaryColor, lineWidth: geometry.size.width * 0.004)
-                )
             }
+            .frame(width: geometry.size.width * 0.38, height: geometry.size.height * 0.28)
+            .padding(geometry.size.height * 0.014)
+            .padding(.vertical, geometry.size.height * 0.005)
+            .background(Color(hex: 0xF6FCFE).opacity(0.8))
+            .border(Color(hex: 0xDFE6E9), width: geometry.size.width * 0.003)
+            .cornerRadius(geometry.size.width * 0.01)
+            .overlay(
+                RoundedRectangle(cornerRadius: geometry.size.width * 0.01)
+                    .stroke(Color(hex: 0xDFE6E9).opacity(0.6), lineWidth: geometry.size.width * 0.004)
+            )
+            .shadow(color: .gray, radius: geometry.size.width * 0.004)
         }
-        .frame(width: geometry.size.width * 0.38, height: geometry.size.height * 0.28)
-        .padding(geometry.size.height * 0.014)
-        .padding(.vertical, geometry.size.height * 0.005)
-        .background(Color(hex: 0xF6FCFE).opacity(0.8))
-        .border(Color(hex: 0xDFE6E9), width: geometry.size.width * 0.003)
-        .cornerRadius(geometry.size.width * 0.01)
-        .overlay(
-            RoundedRectangle(cornerRadius: geometry.size.width * 0.01)
-                .stroke(Color(hex: 0xDFE6E9).opacity(0.6), lineWidth: geometry.size.width * 0.004)
-        )
-        .shadow(color: .gray, radius: geometry.size.width * 0.004)
     }
     private func getDeviceInfo() {
         let url = URL(string: "http://172.20.10.2:5000/get-devices")!
