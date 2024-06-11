85b442398b9f0e0ce9537fd808672f2bd9d58bab
piacobelli14 <dinolabsauthentication@outlook.com>
2024-06-11 12:38:08 -0500

    update

--- NightingaleBookkeeper/ContentView.swift
+++ NightingaleBookkeeper/ContentView.swift
@@ -12,19 +12,56 @@ struct ContentView: View {
     @State private var authenticatedUsername: String = ""
     @State private var authenticatedOrgID: String = ""
     @State private var selectedDeviceID: String = ""
+    @State private var isLoggedOut: Bool = false
     
     var body: some View {
-        switch currentView {
-        case .DeviceManage:
-            DeviceManage(currentView: $currentView, authenticatedUsername: $authenticatedUsername, authenticatedOrgID: $authenticatedOrgID, selectedDeviceID: $selectedDeviceID)
-        case .AddDevice:
-            AddDevice(currentView: $currentView, authenticatedUsername: $authenticatedUsername, authenticatedOrgID: $authenticatedOrgID, selectedDeviceID: $selectedDeviceID)
-        case .RemoveDevice:
-            RemoveDevice(currentView: $currentView, authenticatedUsername: $authenticatedUsername, authenticatedOrgID: $authenticatedOrgID, selectedDeviceID: $selectedDeviceID)
-        case .LoginAuth:
-            LoginAuth(currentView: $currentView, authenticatedUsername: $authenticatedUsername, authenticatedOrgID: $authenticatedOrgID)
-        case .ResetAuth:
-            ResetAuth(currentView: $currentView)
+        Group {
+            switch currentView {
+            case .LoginAuth:
+                LoginAuth(currentView: $currentView, authenticatedUsername: $authenticatedUsername, authenticatedOrgID: $authenticatedOrgID)
+                    .onAppear {
+                        deleteTokenFromKeychain()
+                        authenticatedUsername = ""
+                        authenticatedOrgID = ""
+                        selectedPatientIDs = [""]
+                        selectedEventID = ""
+                    }
+            case .ResetAuth:
+                ResetAuth(currentView: $currentView)
+            case .DeviceManage:
+                DeviceManage(currentView: $currentView, authenticatedUsername: $authenticatedUsername, authenticatedOrgID: $authenticatedOrgID, selectedDeviceID: $selectedDeviceID)
+            case .AddDevice:
+                AddDevice(currentView: $currentView, authenticatedUsername: $authenticatedUsername, authenticatedOrgID: $authenticatedOrgID, selectedDeviceID: $selectedDeviceID)
+            case .RemoveDevice:
+                RemoveDevice(currentView: $currentView, authenticatedUsername: $authenticatedUsername, authenticatedOrgID: $authenticatedOrgID, selectedDeviceID: $selectedDeviceID)
+            }
+        }
+        .onAppear {
+            initializeView()
+        }
+        .onChange(of: currentView) { newView in
+            checkToken()
+        }
+    }
+
+    func checkToken() {
+        if let token = loadTokenFromKeychain() {
+            if isTokenExpired(token: token) {
+                deleteTokenFromKeychain()
+                isLoggedOut = true
+                currentView = .LoginAuth
+            }
+        } else {
+            isLoggedOut = true
+            currentView = .LoginAuth
+        }
+    }
+    
+    func initializeView() {
+        if let token = loadTokenFromKeychain(), !isTokenExpired(token: token) {
+            currentView = .DeviceManage
+        } else {
+            currentView = .LoginAuth
         }
     }
 }
